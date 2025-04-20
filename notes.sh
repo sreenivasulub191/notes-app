e3bdb84d28b68165000534e6d93eb943ad952ba9
sreenivasulu b <sreenuvasulu.b544@gmail.com>
2025-04-21 02:12:46 +0530

    add date to notes display

--- notes.sh
+++ notes.sh
@@ -4,6 +4,9 @@ echo "Welcome to notes app!"
 echo "Your notes:"
 cat notes.txt 2>/dev/null
 
+echo "your notes (as of $(date)):"
+cat notes.txt 2>/dev/null
+
 echo "Enter your note:"
 read note
 echo "$note" >> notes.txt
